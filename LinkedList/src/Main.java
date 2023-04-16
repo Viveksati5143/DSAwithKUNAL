cfce317fcc1655688b2da215d7c945c67c2c1477
VIVEK SATI <viveksati2002@gmail.com>
2023-04-16 13:54:32 +0530

    Singly LinkList

---
+++ LinkedList/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(17);
+        list.insertLast(99);
+        list.insert(100,3);
+        list.display();
+        System.out.println(list.deleteFirst());
+        list.display();
+        System.out.println(list.deleteLast());
+        list.display();
+        System.out.println(list.delete(1));
+        list.display();
+    }
+}
