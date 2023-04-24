597d76dfe54145b99f73f6d8e1e1a8df1731611d
VIVEK SATI <viveksati2002@gmail.com>
2023-04-25 04:41:11 +0530

    Circular LinkList

--- LinkedList/src/Main.java
+++ LinkedList/src/Main.java
@@ -1,18 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        LL list = new LL();
-        list.insertFirst(3);
-        list.insertFirst(2);
-        list.insertFirst(8);
-        list.insertFirst(17);
-        list.insertLast(99);
-        list.insert(100,3);
+//        LL list = new LL();
+//        list.insertFirst(3);
+//        list.insertFirst(2);
+//        list.insertFirst(8);
+//        list.insertFirst(17);
+//        list.insertLast(99);
+//        list.insert(100,3);
+//        list.display();
+//        System.out.println(list.deleteFirst());
+//        list.display();
+//        System.out.println(list.deleteLast());
+//        list.display();
+//        System.out.println(list.delete(1));
+//        list.display();
+//        DLL list = new DLL();
+//        list.insertFirst(3);
+//        list.insertFirst(2);
+//        list.insertFirst(8);
+//        list.insertFirst(17);
+////        list.display();
+////        list.displayRev();
+//        list.insertLast(23);
+//        list.insert(8,65);
+//        list.display();
+
+        CLL list = new CLL();
+        list.insert(23);
+        list.insert(3);
+        list.insert(19);
+        list.insert(75);
+        list.insert(80);
         list.display();
-        System.out.println(list.deleteFirst());
-        list.display();
-        System.out.println(list.deleteLast());
-        list.display();
-        System.out.println(list.delete(1));
+        list.delete(19);
         list.display();
     }
 }
