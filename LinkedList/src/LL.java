4057d1ce6ed6d0c392315e6708579b673b26198c
VIVEK SATI <viveksati2002@gmail.com>
2023-04-27 03:39:32 +0530

    LinkList

--- LinkedList/src/LL.java
+++ LinkedList/src/LL.java
@@ -53,6 +53,20 @@ public class LL {
         size++;
     }
 
+    public void insertRec(int val, int index){
+        head = insertRec(val, index, head);
+    }
+    private Node insertRec(int val, int index, Node node){
+        if (index == 0){
+            Node temp = new Node(val, node);
+            size++;
+            return temp;
+        }
+
+        node.next = insertRec(val, index-1, node.next);
+        return node;
+    }
+
     public void display(){
         Node temp = head;
         while(temp != null){
--- LinkedList/src/Main.java
+++ LinkedList/src/Main.java
@@ -1,19 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-//        LL list = new LL();
-//        list.insertFirst(3);
-//        list.insertFirst(2);
-//        list.insertFirst(8);
-//        list.insertFirst(17);
-//        list.insertLast(99);
-//        list.insert(100,3);
-//        list.display();
-//        System.out.println(list.deleteFirst());
-//        list.display();
-//        System.out.println(list.deleteLast());
-//        list.display();
-//        System.out.println(list.delete(1));
-//        list.display();
+        LL list = new LL();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(17);
+        list.insertLast(99);
+        list.insert(100,3);
+        list.display();
+        System.out.println(list.deleteFirst());
+        list.display();
+        System.out.println(list.deleteLast());
+        list.display();
+        System.out.println(list.delete(1));
+        list.display();
+        list.insertRec(88, 2);
+        list.display();
 //        DLL list = new DLL();
 //        list.insertFirst(3);
 //        list.insertFirst(2);
@@ -25,14 +27,14 @@ public class Main {
 //        list.insert(8,65);
 //        list.display();
 
-        CLL list = new CLL();
-        list.insert(23);
-        list.insert(3);
-        list.insert(19);
-        list.insert(75);
-        list.insert(80);
-        list.display();
-        list.delete(19);
-        list.display();
+//        CLL list = new CLL();
+//        list.insert(23);
+//        list.insert(3);
+//        list.insert(19);
+//        list.insert(75);
+//        list.insert(80);
+//        list.display();
+//        list.delete(19);
+//        list.display();
     }
 }
